2370340506c3a3c749d7b74d73bcbd435449fb42
Pedro Alex <piedroalex@gmail.com>
2022-11-29 12:50:11 -0300

    Models, enums e dtos de email criados.

---
+++ src/main/java/br/com/palm/email/models/Email.java
@@ -0,0 +1,35 @@
+package br.com.palm.email.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import br.com.palm.email.enums.StatusEmail;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data @Entity @Table(name = "TB_EMAIL")
+public class Email implements Serializable {
+	
+    private static final long serialVersionUID = 1L;
+
+    @Id @GeneratedValue(strategy= GenerationType.AUTO)
+    private UUID emailId;
+    
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+    
+}
